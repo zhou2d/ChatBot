6e7b8a38423415ecc328cbcda269919610dae07b
Dake Zhou <zhou2d@yahoo.com>
2024-08-16 19:47:08 +1200

    Modified for dashcope

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, request, jsonify
-
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+from flask import Flask, render_template, request
+from http import HTTPStatus
+import markdown
+from dashscope import Application
 
 
 app = Flask(__name__)
@@ -17,27 +12,18 @@ def index():
 
 
 @app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
+def get_Chat_response():
+    response = Application.call(app_id='9193d7ac66de4b79bd7f5d78db494f95',
+                                prompt=request.form["msg"],
+                                api_key='sk-ca86b34dfd6242a3abd028a47e08dc2b',
+                                )
 
+    #print (markdown.markdown(response.output.text))                               
 
-def get_Chat_response(text):
-
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    if response.status_code != HTTPStatus.OK:
+        return 'request_id=%s, code=%s, message=%s\n' % (response.request_id, response.status_code, response.message)
+    else:
+        return markdown.markdown(response.output.text)
 
 
 if __name__ == '__main__':
